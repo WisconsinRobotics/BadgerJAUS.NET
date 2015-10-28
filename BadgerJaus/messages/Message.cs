1f7d5339ccb5d7bde5ab8eb29530abdea395f522
Ziliang Guo <zguo2@wisc.edu>
2015-10-27 21:07:34 -0500

    Use proper null check.

--- BadgerJaus/messages/Message.cs
+++ BadgerJaus/messages/Message.cs
@@ -326,7 +326,7 @@ namespace BadgerJaus.Messages
         protected virtual bool PayloadToJausBuffer(byte[] data, int index, out int indexOffset)
         {
             indexOffset = index;
-            if (this.data == null)
+            if (this.data != null)
             {
                 Array.Copy(this.data, 0, data, index, this.data.Length);
                 indexOffset += this.data.Length;
