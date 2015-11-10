16decd6ed91b00528b2cfb97c2ecfc4a9bdb519a
William Jen <wjen@wisc.edu>
2015-11-10 09:41:35 -0600

    FixedVelocityStateDriver messages to set the appropriate presence vector bit. Added license headers.

--- BadgerJaus/messages/VelocityStateDriver/ReportVelocityCommand.cs
+++ BadgerJaus/messages/VelocityStateDriver/ReportVelocityCommand.cs
@@ -1,8 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ * Copyright (c) 2015, Wisconsin Robotics
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * * Neither the name of Wisconsin Robotics nor the
+ *   names of its contributors may be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ *   
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL WISCONSIN ROBOTICS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 using BadgerJaus.Util;
 
@@ -82,37 +103,61 @@ namespace BadgerJaus.Messages.VelocityStateDriver
         public double VelocityX
         {
             get { return velocityX.ScaleValueToDouble(LINEAR_MIN, LINEAR_MAX); }
-            set { velocityX.SetValueFromDouble(value, LINEAR_MIN, LINEAR_MAX); }
+            set
+            {
+                presenceVector.ToggleBit(VELOCITY_X_BIT, true);
+                velocityX.SetValueFromDouble(value, LINEAR_MIN, LINEAR_MAX); 
+            }
         }
 
         public double VelocityY
         {
             get { return velocityY.ScaleValueToDouble(LINEAR_MIN, LINEAR_MAX); }
-            set { velocityY.SetValueFromDouble(value, LINEAR_MIN, LINEAR_MAX); }
+            set 
+            {
+                presenceVector.ToggleBit(VELOCITY_Y_BIT, true);
+                velocityY.SetValueFromDouble(value, LINEAR_MIN, LINEAR_MAX);
+            }
         }
 
         public double VelocityZ
         {
             get { return velocityZ.ScaleValueToDouble(LINEAR_MIN, LINEAR_MAX); }
-            set { velocityZ.SetValueFromDouble(value, LINEAR_MIN, LINEAR_MAX); }
+            set
+            {
+                presenceVector.ToggleBit(VELOCITY_Z_BIT, true);
+                velocityZ.SetValueFromDouble(value, LINEAR_MIN, LINEAR_MAX); 
+            }
         }
 
         public double RollRate
         {
             get { return rollRate.ScaleValueToDouble(ANGULAR_MIN, ANGULAR_MAX); }
-            set { rollRate.SetValueFromDouble(value, ANGULAR_MIN, ANGULAR_MAX); }
+            set 
+            {
+                presenceVector.ToggleBit(ROLL_RATE_BIT, true);
+                rollRate.SetValueFromDouble(value, ANGULAR_MIN, ANGULAR_MAX); 
+            }
         }
 
         public double PitchRate
         {
             get { return pitchRate.ScaleValueToDouble(ANGULAR_MIN, ANGULAR_MAX); }
-            set { pitchRate.SetValueFromDouble(value, ANGULAR_MIN, ANGULAR_MAX); }
+            set
+            {
+                presenceVector.ToggleBit(PITCH_RATE_BIT, true);
+                pitchRate.SetValueFromDouble(value, ANGULAR_MIN, ANGULAR_MAX);
+            }
         }
 
         public double YawRate
         {
             get { return yawRate.ScaleValueToDouble(ANGULAR_MIN, ANGULAR_MAX); }
-            set { yawRate.SetValueFromDouble(value, ANGULAR_MIN, ANGULAR_MAX); }
+            set 
+            {
+                presenceVector.ToggleBit(YAW_RATE_BIT, true);
+                yawRate.SetValueFromDouble(value, ANGULAR_MIN, ANGULAR_MAX); 
+            }
         }
     }
 }
--- BadgerJaus/services/mobility/VelocityStateDriver.cs
+++ BadgerJaus/services/mobility/VelocityStateDriver.cs
@@ -1,9 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
+﻿/*
+ * Copyright (c) 2015, Wisconsin Robotics
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * * Neither the name of Wisconsin Robotics nor the
+ *   names of its contributors may be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ *   
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL WISCONSIN ROBOTICS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 using BadgerJaus.Messages;
 using BadgerJaus.Messages.VelocityStateDriver;
@@ -15,10 +35,10 @@ namespace BadgerJaus.Services.Mobility
 {
     public class VelocityStateDriver : BaseService
     {
-        public const String SERVICE_NAME = "VelocityStateDriver";
-        public const String SERVICE_VERSION = "1.0";
-        public const String SERVICE_ID = "urn:jaus:jss:mobility:VelocityStateDriver";
-        public const String PARENT_SERVICE = "Management";
+        public const string SERVICE_NAME = "VelocityStateDriver";
+        public const string SERVICE_VERSION = "1.0";
+        public const string SERVICE_ID = "urn:jaus:jss:mobility:VelocityStateDriver";
+        public const string PARENT_SERVICE = "Management";
 
         protected double velocityX;
         protected double velocityY;
