3864e7383be4da30abb6f784a2b60c2324b3f222
Ziliang Guo <zguo2@wisc.edu>
2016-02-17 21:49:32 -0600

    Add test instantiation of a JAUS subsystem.

---
+++ BadgerJAUSSubsystem/BadgerJAUSSubsystem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BadgerJaus.Util;
+
+namespace BadgerJAUSSubsystem
+{
+    class BadgerJAUSSubsystem : JausSubsystem
+    {
+        const int SUBSYSTEM_ID = 10;
+
+        BadgerJAUSSubsystem() : base(SUBSYSTEM_ID, "BadgerJAUSSubsystem")
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            BadgerJAUSSubsystem badgerJAUSSubsystem = new BadgerJAUSSubsystem();
+        }
+    }
+}
